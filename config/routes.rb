832b8b04ee31b001c921590dc175fbc8882fc1c7
Nicholas Weber <nicholaseweber@gmail.com>
2022-02-11 09:07:33 -0600

    Add routes and links between pages
    Chapter 11

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root "events#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "events" => "events#index"
-  get "events/:id" => "events#show"
+  get "events/:id" => "events#show", as: "event"
 end
