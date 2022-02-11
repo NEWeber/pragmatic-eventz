e6cfd577f03c7d3e110c2db2e5d1985fcd7f4843
Nicholas Weber <nicholaseweber@gmail.com>
2022-02-11 10:27:40 -0600

    Create form for editing
    12.1

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "events" => "events#index"
   get "events/:id" => "events#show", as: "event"
+  get "events/:id/edit" => "events#edit", as: "edit_event"
+
 end
