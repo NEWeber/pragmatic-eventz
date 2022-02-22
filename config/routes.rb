6aa1ff338545954f0f0ff9ee17969d65a796119d
Nicholas Weber <nicholaseweber@gmail.com>
2022-02-22 10:42:07 -0600

    Add sign up form and actions
    Chapter 27

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
   root "events#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :events do
     resources :registrations
   end
+
+  resources :users
+  get "signup" => "users#new"
 end
