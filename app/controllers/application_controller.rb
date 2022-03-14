5e2f7d5813a96c72750224e98b373336b6a6edcf
Nicholas Weber <nicholaseweber@gmail.com>
2022-03-14 15:03:42 -0500

    Add admin user features
    Chapter 34

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,4 +20,16 @@ private
 
   helper_method :current_user?
 
+  def require_admin
+    unless current_user_admin?
+      redirect_to events_url, alert: "Unauthorized access!"
+    end
+  end
+
+  def current_user_admin?
+    current_user && current_user.admin?
+  end
+
+  helper_method :current_user_admin?
+
 end
