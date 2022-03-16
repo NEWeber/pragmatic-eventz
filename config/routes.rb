452a0790c5d462786f46d4c75f1628c51bd4559a
Nicholas Weber <nicholaseweber@gmail.com>
2022-03-16 16:05:43 -0500

    Add like and unlike button and function
    Chapter 37.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :likes
   root "events#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :events do
     resources :registrations
+    resources :likes
   end
 
   resource :session, only: [:new, :create, :destroy]
