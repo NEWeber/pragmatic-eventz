a6c0b402fb5c3a369fe0a8f30dad9664b11a012a
Nicholas Weber <nicholaseweber@gmail.com>
2022-03-22 13:57:38 -0500

    Add custom filtering routes
    Chapter 39.2

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :categories
   root "events#index"
 
+
+  get "events/filter/:filter" => "events#index", as: :filtered_events
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :events do
     resources :registrations
